f4b1413b1ac89541fbf82d3d47675002cff18ed5
Shakeel Ahmad <shakeel2717@gmail.com>
2023-07-16 18:52:23 +0500

    first commit

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+$config['db']['host'] = 'localhost';
+$config['db']['name'] = 'openai';
+$config['db']['user'] = 'root';
+$config['db']['pass'] = '';
+$config['db']['pre'] = 'qa_';
+
+$config['admin_folder'] = 'admin';
+$config['version'] = '3.9';
+$config['installed'] = '1';
+?>
